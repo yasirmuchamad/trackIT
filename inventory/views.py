402a7a214f3782489b5bfed222604d0e99e3eb26
yassir <muchamad445@gmail.com>
2025-07-23 04:01:52 +0700

    add views.py, and define listView

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def listCategory(request):
+    category = Category.objects.all()
+
+    context = {
+        'title'     :'list category',
+        'categories':category,
+    }
+
+    return render(request, 'inventory/category/list.html', context)
