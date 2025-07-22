26c6cc2f05829e7c5c7a461fb702ee2e05e3d9f9
yassir <muchamad445@gmail.com>
2025-07-23 00:25:24 +0700

    add models Category and Item

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    """Model definition for Category."""
+
+    # TODO: Define fields here
+    name = models.CharField(max_length=32)
+     
+    class Meta:
+        """Meta definition for Category."""
+
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categorys'
+
+    def __str__(self):
+        """Unicode representation of Category."""
+        return f"{self.nama}"
+
+
+class Item(models.Model):
+    """Model definition for Item."""
+    name = models.CharField(max_length = 100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    brand = models.CharField(max_length=50)
+    entry_date = models.DateTimeField(default = timezone.now)
+
+    # TODO: Define fields here
+
+    class Meta:
+        """Meta definition for Item."""
+
+        verbose_name = 'Item'
+        verbose_name_plural = 'Items'
+
+    def __str__(self):
+        """Unicode representation of Item."""
+        return f"{self.name} {self.brand}"
