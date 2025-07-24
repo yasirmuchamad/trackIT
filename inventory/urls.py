7efea8d9c3de83480721378472b147ab7ace33c5
yassir <muchamad445@gmail.com>
2025-07-24 08:20:25 +0700

    update category_list to list_category, tidying list.html with css

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,7 +3,8 @@ from .views import *
 
 app_name = 'inventory'
 urlpatterns = [
-    path('category/', CategoryListView.as_view(), name='category_list'),
+    path('category/', CategoryListView.as_view(), name='list_category'),
     path('category/create', CategoryCreateView.as_view(), name='create_category'),
-    path('category/update/<int:pk>', CategoryUpdateView.as_view(), name='create_category'),
+    path('category/update/<int:pk>', CategoryUpdateView.as_view(), name='update_category'),
+    path('category/delete/<int:pk>', CategoryDeleteView.as_view(), name='delete_category'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,7 @@
 from inventory.forms import CategoryForm
 from inventory.models import Category
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 
 class CategoryListView(ListView):
@@ -30,9 +30,21 @@ class CategoryUpdateView(UpdateView):
     model = Category
     form_class = CategoryForm
     template_name = "inventory/category/create.html"
-    success_url = reverse_lazy('inventory:category_list')
+    success_url = reverse_lazy('inventory:list_category')
 
     def get_context_data(self, **kwrags):
         context = super().get_context_data(**kwrags)
         context['title']="Update Category"
         return context
+
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    template_name = "inventory/category/delete.html"
+    success_url = reverse_lazy('inventory:list_category')
+
+    def delete(self, request, *args, **kwargs):
+        category = self.get_object()
+        messages.success(self.request, f"Category '{category.name}' berhasil disimpan.")
+        return super().delete(request, *args, **kwargs)
+                                                    
