366e8bb5e004c2c880809dbdb506d6b177047165
yassir <muchamad445@gmail.com>
2025-12-21 18:44:40 +0700

    add path onboardingSubmitAPIView

--- employee/urls.py
+++ employee/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import *
+from employee.api.onboarding import OnboardingSubmitAPIView
 
 app_name = 'employees'
 urlpatterns = [
+    path("api/onboarding/submit/<uuid:token>/", 
+         OnboardingSubmitAPIView.as_view(),
+         name="onboarding-submit"),
+
     # path('/', CategoryListView.as_view(), name='list_category'),
     # path('category/create', CategoryCreateView.as_view(), name='create_category'),
     # path('category/update/<int:pk>', CategoryUpdateView.as_view(), name='update_category'),
